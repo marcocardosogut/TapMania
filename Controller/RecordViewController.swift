d6aae0f141c55d79be75cb791a82ceb4de832611
Marco Cardoso <marco.cardosogut@ufl.edu>
2018-12-26 14:28:10 -0500

    Fixing Records

--- Controller/RecordViewController.swift
+++ Controller/RecordViewController.swift
@@ -12,14 +12,14 @@ class RecordViewController: UIViewController {
     //********* VISUALS *********
     @IBOutlet weak var button_Background: UIButton!
     @IBOutlet weak var button_Back: UIButton!
-    @IBOutlet weak var label_Easy: UILabel!
     @IBOutlet weak var label_EasyScore: UILabel!
-    @IBOutlet weak var label_Medium: UILabel!
     @IBOutlet weak var label_MediumScore: UILabel!
-    @IBOutlet weak var label_Hard: UILabel!
     @IBOutlet weak var label_HardScore: UILabel!
-    @IBOutlet weak var label_Unfair: UILabel!
     @IBOutlet weak var label_UnfairScore: UILabel!
+    @IBOutlet weak var view_Easy: UIView!
+    @IBOutlet weak var view_Medium: UIView!
+    @IBOutlet weak var view_Hard: UIView!
+    @IBOutlet weak var view_Unfair: UIView!
     //********* END VISUALS *********
     
     private let defaults: UserDefaults = UserDefaults.init()    //Stores the app defaults values. Used to store settigns and records
@@ -73,12 +73,12 @@ class RecordViewController: UIViewController {
     //Make adjustments to fit the content in display of iPhone 5 and SE
     private func adjustVisual(){
         if(UIDevice.current.name.contains("SE") || UIDevice.current.name.contains("5")){
-            label_Medium.topAnchor.constraint(equalTo: label_Easy.bottomAnchor, constant: 10).isActive = true
-            label_MediumScore.topAnchor.constraint(equalTo: label_EasyScore.bottomAnchor, constant: 10).isActive = true
-            label_Hard.topAnchor.constraint(equalTo: label_Medium.bottomAnchor, constant: 10).isActive = true
-            label_HardScore.topAnchor.constraint(equalTo: label_MediumScore.bottomAnchor, constant: 10).isActive = true
-            label_Unfair.topAnchor.constraint(equalTo: label_Hard.bottomAnchor, constant: 10).isActive = true
-            label_UnfairScore.topAnchor.constraint(equalTo: label_HardScore.bottomAnchor, constant: 10).isActive = true
+
+            
+            view_Medium.topAnchor.constraint(equalTo: view_Easy.bottomAnchor, constant: -15).isActive = true
+            view_Hard.topAnchor.constraint(equalTo: view_Medium.bottomAnchor, constant: -15).isActive = true
+            view_Unfair.topAnchor.constraint(equalTo: view_Hard.bottomAnchor, constant: -15).isActive = true
+            
         }
     }
     
